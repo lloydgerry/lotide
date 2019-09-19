c9d0dcc6637912a278add68f8b30f75b0b733327
Lloyd <lloyd.gerry@gmail.com>
2019-09-19 22:23:30 +0000

    adding map.js

---
+++ map.js
@@ -0,0 +1,22 @@
+// inputs:
+//        an array
+//        callback function
+// outputs:
+//         return new away based on callback
+
+const words = ["Paris", "Rome", "New York", "Montreal"];
+
+const map = function(array, callback) {
+  const results = [];
+
+  for (let item of array) {
+    console.log('item BEFORE: ', item);
+    results.push(callback(item))
+    console.log('item AFTER: ',  callback(item));
+  }
+
+  return results;
+}
+
+const results1 = map(words, word => word);
+console.log(results1);
