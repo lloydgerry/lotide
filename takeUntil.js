518466a7c24c6240412b7c2ebaeec66c845a5025
Lloyd <lloyd.gerry@gmail.com>
2019-09-20 00:17:27 +0000

    adding takeUntil.js

---
+++ takeUntil.js
@@ -0,0 +1,27 @@
+// input(s):
+//          array 
+//          callback
+//output(s):
+//         slice of OG array until it reaches callback
+
+const takeUntil = function(array, callback) {
+  const results = []
+  //console.log("arraybefore: ", array);
+  for (let element of array) {
+    console.log(array)
+    if (!callback(element)) {
+      results.push(element);
+    } else {
+      break;
+    }
+    }
+    return results;
+  }
+
+const countries = ["Greenland", "Canada", "England", "Spain", "Germany", "Argentina"];
+
+const data1 = [1, 2, 3, 4]
+
+const results1 = takeUntil(data1, stop => stop > 3)
+
+console.log(results1);
