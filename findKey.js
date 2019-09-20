73f5a38637f2867c199693e598bb25498eacbc4e
Lloyd <lloyd.gerry@gmail.com>
2019-09-20 01:38:55 +0000

    adding findKey.js

---
+++ findKey.js
@@ -0,0 +1,27 @@
+
+const findKey = function(object, callback) {
+  let allObjectValues = Object.keys(object)
+  for (let key of allObjectValues) {
+    //console.log("key: ", object[key])
+    //console.log("callback: ", callback(object[key]));
+    if (callback(object[key]))  {
+      //console.log("key: ", callback(object[key]));
+      return key;
+    } 
+    }
+      return undefined;
+}
+
+const result1 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => {
+  console.log("x: ", x);
+  return x.stars === 2;
+}) // => "noma"
+
+console.log(result1);
