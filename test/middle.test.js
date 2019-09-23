c9c34539a73841e213244a8ed8a9c64c20bb46f8
Lloyd <lloyd.gerry@gmail.com>
2019-09-23 21:36:35 +0000

    adding chaitests to middle and tail js files

--- test/middle.test.js
+++ test/middle.test.js
@@ -1,12 +1,27 @@
 
 //add dependancies
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle')
 
 //test cases
 
-assertArraysEqual(middle([1,2,3]), [2]);
-assertArraysEqual(middle([1,2,3,4]), [2,3]);
-assertArraysEqual(middle([1,2,]), []);
-assertArraysEqual(middle([1]), []);
+// assertArraysEqual(middle([1,2,3]), [2]);
+// assertArraysEqual(middle([1,2,3,4]), [2,3]);
+// assertArraysEqual(middle([1,2,]), []);
+// assertArraysEqual(middle([1]), []);
 
+
+describe("#middle", () => {
+  it("should return middle index of 2 from array of 1,2,3", () => {
+    assert.deepEqual(middle([1,2,3]), [2]);
+  });
+  it("should return middle two indexs of 2,3, from array of 1,2,3,4", () => {
+    assert.deepEqual(middle([1,2,3,4]), [2,3])
+  });
+  it("should return middle two indexs of 2,3, from array of 1,2,3,4", () => {
+    assert.deepEqual(middle([1,2]), [])
+  });  
+  it("should return middle two indexs of 2,3, from array of 1,2,3,4", () => {
+    assert.deepEqual(middle([1]), [])
+  });
+});
--- test/tail.test.js
+++ test/tail.test.js
@@ -1,8 +1,23 @@
 
 // add assert equals
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const tail = require('../tail.js');
 
 
 // TEST CODE
-assertArraysEqual(tail([1, 2, 3]), [2, 3]);
+// assertArraysEqual(tail([1, 2, 3]), [2, 3]);
+
+describe("#tail", () => {
+  it("should return tail of 2,3, from array of 1,2,3", () => {
+    assert.deepEqual(tail([1,2,3]), [2,3]);
+  });
+  it("should return tail two indexs of 2,3, from array of 1,2,3,4", () => {
+    assert.deepEqual(tail([1,2,3,4]), [2,3,4])
+  });
+  it("should return tail two indexs of 2,3, from array of 1,2,3,4", () => {
+    assert.deepEqual(tail([1,2]), [2])
+  });  
+  it("should return tail two indexs of 2,3, from array of 1,2,3,4", () => {
+    assert.deepEqual(tail([1]), [])
+  });
+});
